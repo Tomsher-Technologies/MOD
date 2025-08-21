b240eda1e6b42acb1f9b10e88861291575045113
Jisha <jisha.tomsher@gmail.com>
2025-08-21 11:50:07 +0400

    db changes

--- db_changes.sql
+++ db_changes.sql
@@ -1,3 +1,3 @@
 -- INSERT INTO `dropdowns` (`id`, `name`, `code`, `status`, `created_at`, `updated_at`) VALUES (NULL, 'Room Type', 'room_type', '1', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
-ALTER TABLE `dropdown_options` CHANGE `sort_order` `sort_order` INT NULL DEFAULT '0';
+-- ALTER TABLE `dropdown_options` CHANGE `sort_order` `sort_order` INT NULL DEFAULT '0';
