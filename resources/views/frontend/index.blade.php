457f377bf867c77eb59bc25a3c58b4f046cd06c7
Jisha <jisha.tomsher@gmail.com>
2025-09-27 09:42:19 +0400

    changes

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -4,7 +4,7 @@
     <section class="w-full h-[100vh] relative">
         <!-- Background Video -->
         <video autoplay muted loop playsinline class="w-full h-full object-cover -z-20">
-            <source src="{{ asset(getUploadedImage($page->getTranslation('link', $lang))) }}" type="video/mp4" />
+            <source src="{{ asset(getUploadedImage($page?->getTranslation('link', $lang))) }}" type="video/mp4" />
             Your browser does not support the video tag.
         </video>
         <!-- Top Gradient -->
@@ -16,13 +16,13 @@
         <!-- Text Content -->
         <div class="absolute inset-0 z-20 flex justify-start items-end px-6 pb-[80px]">
             <div class="container mx-auto">
-                <h2 class="text-[80px] md:text-6xl font-bold text-white mb-4">{{ $page->getTranslation('title1', $lang) }}</h2>
+                <h2 class="text-[80px] md:text-6xl font-bold text-white mb-4">{{ $page?->getTranslation('title1', $lang) }}</h2>
                 <p class="text-white text-[28px] mb-6">
-                    {{ $page->getTranslation('title2', $lang) }}
+                    {{ $page?->getTranslation('title2', $lang) }}
                 </p>
                 <a href="{{ route('about-us') }}"
                     class="inline-block bg-[#b68a35] text-white px-8 py-3 rounded-lg hover:bg-[#9e7526] transition">
-                    {{ $page->getTranslation('btn_link_1', $lang) }}
+                    {{ $page?->getTranslation('btn_link_1', $lang) }}
                 </a>
             </div>
         </div>
@@ -35,15 +35,15 @@
             <div class="grid grid-cols-12">
                 <div class="col-span-6 pe-[100px]">
                     <h2 class="text-[40px] leading-[40px] text-[#744e2e] mb-8">
-                        {{ $page->getTranslation('title3', $lang) }}
+                        {{ $page?->getTranslation('title3', $lang) }}
                     </h2>
                 </div>
                 <div class="col-span-6 text-[#797E86]">
-                    {!! $page->getTranslation('content1', $lang) !!}
+                    {!! $page?->getTranslation('content1', $lang) !!}
                     <br>
                     <a href="{{ route('about-us') }}"
                         class="inline-block bg-[#b68a35] mt-4 text-white px-8 py-3 rounded-lg hover:bg-[#9e7526] transition">
-                        {!! $page->getTranslation('btn_link_2', $lang) !!}
+                        {!! $page?->getTranslation('btn_link_2', $lang) !!}
                     </a>
                 </div>
             </div>
@@ -54,14 +54,14 @@
         <div class="container mx-auto ">
             <div class="grid grid-cols-12">
                 <div class="col-span-6 pe-[100px]">
-                    <h2 class="text-[40px] leading-[40px] text-[#744e2e] mb-8"> {!! $page->getTranslation('title4', $lang) !!}</h2>
+                    <h2 class="text-[40px] leading-[40px] text-[#744e2e] mb-8"> {!! $page?->getTranslation('title4', $lang) !!}</h2>
                     <hr class="border-[#ebebea] mb-8" />
-                    {!! $page->getTranslation('content2', $lang) !!}
+                    {!! $page?->getTranslation('content2', $lang) !!}
                 </div>
                 <div class="grid gap-6 md:grid-cols-2 lg:grid-cols-2 col-span-6">
                     <!-- Contact Card -->
                     <div class="p-6 rounded-lg border border-[#b68a35]">
-                        <h3 class="text-2xl text-[#b68a35] font-medium mb-4">{!! $page->getTranslation('title5', $lang) !!}</h3>
+                        <h3 class="text-2xl text-[#b68a35] font-medium mb-4">{!! $page?->getTranslation('title5', $lang) !!}</h3>
                         <hr class="border-[#ebebea] mb-6" />
                         <div class="flex items-center gap-3">
                             <svg class="w-10 h-10 text-[#797E86]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
@@ -70,12 +70,12 @@
                                 <path
                                     d="M18.427 14.768 17.2 13.542a1.733 1.733 0 0 0-2.45 0l-.613.613a1.732 1.732 0 0 1-2.45 0l-1.838-1.84a1.735 1.735 0 0 1 0-2.452l.612-.613a1.735 1.735 0 0 0 0-2.452L9.237 5.572a1.6 1.6 0 0 0-2.45 0c-3.223 3.2-1.702 6.896 1.519 10.117 3.22 3.221 6.914 4.745 10.12 1.535a1.601 1.601 0 0 0 0-2.456Z" />
                             </svg>
-                            <a href="tel:{!! $page->getTranslation('content5', $lang) !!}"
-                                class="text-lg text-[#797E86] font-medium hover:text-[#b68a35] transition">{!! $page->getTranslation('content5', $lang) !!}</a>
+                            <a href="tel:{!! $page?->getTranslation('content5', $lang) !!}"
+                                class="text-lg text-[#797E86] font-medium hover:text-[#b68a35] transition">{!! $page?->getTranslation('content5', $lang) !!}</a>
                         </div>
                     </div>
                     <div class="p-6 rounded-lg border border-[#b68a35]">
-                        <h3 class="text-2xl text-[#b68a35] font-medium mb-4">{!! $page->getTranslation('title6', $lang) !!}</h3>
+                        <h3 class="text-2xl text-[#b68a35] font-medium mb-4">{!! $page?->getTranslation('title6', $lang) !!}</h3>
                         <hr class="border-[#ebebea] mb-6" />
                         <div class="flex items-center gap-3">
                             <svg class="w-10 h-10 text-[#797E86]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
@@ -84,12 +84,12 @@
                                 <path
                                     d="M18.427 14.768 17.2 13.542a1.733 1.733 0 0 0-2.45 0l-.613.613a1.732 1.732 0 0 1-2.45 0l-1.838-1.84a1.735 1.735 0 0 1 0-2.452l.612-.613a1.735 1.735 0 0 0 0-2.452L9.237 5.572a1.6 1.6 0 0 0-2.45 0c-3.223 3.2-1.702 6.896 1.519 10.117 3.22 3.221 6.914 4.745 10.12 1.535a1.601 1.601 0 0 0 0-2.456Z" />
                             </svg>
-                            <a href="tel:{!! $page->getTranslation('content6', $lang) !!}"
-                                class="text-lg text-[#797E86] font-medium hover:text-[#b68a35] transition">{!! $page->getTranslation('content6', $lang) !!}</a>
+                            <a href="tel:{!! $page?->getTranslation('content6', $lang) !!}"
+                                class="text-lg text-[#797E86] font-medium hover:text-[#b68a35] transition">{!! $page?->getTranslation('content6', $lang) !!}</a>
                         </div>
                     </div>
                     <div class="p-6 rounded-lg border border-[#b68a35]">
-                        <h3 class="text-2xl text-[#b68a35] font-medium mb-4">{!! $page->getTranslation('title7', $lang) !!}</h3>
+                        <h3 class="text-2xl text-[#b68a35] font-medium mb-4">{!! $page?->getTranslation('title7', $lang) !!}</h3>
                         <hr class="border-[#ebebea] mb-6" />
                         <div class="flex items-center gap-3">
                             <svg class="w-10 h-10 text-[#797E86]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
@@ -98,12 +98,12 @@
                                 <path
                                     d="M18.427 14.768 17.2 13.542a1.733 1.733 0 0 0-2.45 0l-.613.613a1.732 1.732 0 0 1-2.45 0l-1.838-1.84a1.735 1.735 0 0 1 0-2.452l.612-.613a1.735 1.735 0 0 0 0-2.452L9.237 5.572a1.6 1.6 0 0 0-2.45 0c-3.223 3.2-1.702 6.896 1.519 10.117 3.22 3.221 6.914 4.745 10.12 1.535a1.601 1.601 0 0 0 0-2.456Z" />
                             </svg>
-                            <a href="tel:{!! $page->getTranslation('content7', $lang) !!}"
-                                class="text-lg text-[#797E86] font-medium hover:text-[#b68a35] transition">{!! $page->getTranslation('content7', $lang) !!}</a>
+                            <a href="tel:{!! $page?->getTranslation('content7', $lang) !!}"
+                                class="text-lg text-[#797E86] font-medium hover:text-[#b68a35] transition">{!! $page?->getTranslation('content7', $lang) !!}</a>
                         </div>
                     </div>
                     <div class="p-6 rounded-lg border border-[#b68a35]">
-                        <h3 class="text-2xl text-[#b68a35] font-medium mb-4">{!! $page->getTranslation('title8', $lang) !!}</h3>
+                        <h3 class="text-2xl text-[#b68a35] font-medium mb-4">{!! $page?->getTranslation('title8', $lang) !!}</h3>
                         <hr class="border-[#ebebea] mb-6" />
                         <div class="flex items-center gap-3">
                             <svg class="w-10 h-10 text-[#797E86]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
@@ -112,28 +112,28 @@
                                 <path
                                     d="M18.427 14.768 17.2 13.542a1.733 1.733 0 0 0-2.45 0l-.613.613a1.732 1.732 0 0 1-2.45 0l-1.838-1.84a1.735 1.735 0 0 1 0-2.452l.612-.613a1.735 1.735 0 0 0 0-2.452L9.237 5.572a1.6 1.6 0 0 0-2.45 0c-3.223 3.2-1.702 6.896 1.519 10.117 3.22 3.221 6.914 4.745 10.12 1.535a1.601 1.601 0 0 0 0-2.456Z" />
                             </svg>
-                            <a href="tel:{!! $page->getTranslation('content8', $lang) !!}"
-                                class="text-lg text-[#797E86] font-medium hover:text-[#b68a35] transition">{!! $page->getTranslation('content8', $lang) !!}</a>
+                            <a href="tel:{!! $page?->getTranslation('content8', $lang) !!}"
+                                class="text-lg text-[#797E86] font-medium hover:text-[#b68a35] transition">{!! $page?->getTranslation('content8', $lang) !!}</a>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </section>
-    <section class="py-[160px]  bg-cover bg-center relative" style="background-image: url('{{ asset($page->getTranslation('image', $lang)) }}');">
+    <section class="py-[160px]  bg-cover bg-center relative" style="background-image: url('{{ asset($page?->getTranslation('image', $lang)) }}');">
         <div class="container mx-auto relative z-10">
-            <h2 class="text-[40px] text-[#744e2e] mb-2">{!! $page->getTranslation('content3', $lang) !!}</h2>
-            <p class="text-[#797E86]">{!! $page->getTranslation('content4', $lang) !!}
+            <h2 class="text-[40px] text-[#744e2e] mb-2">{!! $page?->getTranslation('content3', $lang) !!}</h2>
+            <p class="text-[#797E86]">{!! $page?->getTranslation('content4', $lang) !!}
             </p>
             <a href="{{ route('committees') }}"
                 class="mt-3 inline-block bg-[#b68a35] text-white px-8 py-3 rounded-lg hover:bg-[#9e7526] text-end me-auto">
-                {!! $page->getTranslation('btn_link_3', $lang) !!}
+                {!! $page?->getTranslation('btn_link_3', $lang) !!}
             </a>
         </div>
     </section>
     <section class="bg-white mb-8 py-[100px]">
         <div class="container mx-auto">
-            <h2 class="text-[40px] text-[#744e2e] mb-6">{!! $page->getTranslation('title9', $lang) !!}</h2>
+            <h2 class="text-[40px] text-[#744e2e] mb-6">{!! $page?->getTranslation('title9', $lang) !!}</h2>
             <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-3 gap-6">
                 @foreach ($news as $new)
                     <div class="p-4 rounded-lg  border border-gray-200">
@@ -150,7 +150,7 @@
             <div class="mt-6 text-center">
                 <a href="{{ route('news') }}"
                     class="mt-3 inline-block bg-[#b68a35] text-white px-8 py-3 rounded-lg hover:bg-[#9e7526] text-end me-auto">
-                    {!! $page->getTranslation('btn_link_4', $lang) !!}
+                    {!! $page?->getTranslation('btn_link_4', $lang) !!}
                 </a>
             </div>
         </div>
